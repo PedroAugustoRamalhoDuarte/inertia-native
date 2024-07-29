e398e2c7841dc22948fadad985e19c9decd3b437
PedroAugustoRamalhoDuarte <pedroaugustorduarte@gmail.com>
2024-07-29 10:43:57 -0300

    install tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./app/**/*.html",
+    "./app/frontend/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
